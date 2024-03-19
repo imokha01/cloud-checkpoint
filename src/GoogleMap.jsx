d011de666e24905df72ebeabb7656290ca8ab60b
Ahmed Wahab <ahmed.wahab.imokha@gmail.com>
2024-03-19 15:37:31 +0100

    my cloud checkpoint

---
+++ src/GoogleMap.jsx
@@ -0,0 +1,22 @@
+import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
+
+export default function GoogleMap() {
+  const position = [51.505, -0.09];
+  return (
+    <MapContainer
+      center={position}
+      zoom={13}
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
